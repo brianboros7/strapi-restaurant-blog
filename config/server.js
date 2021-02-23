cf59d675d1dd1bd08ad727553a099d1cd316ffef
Brian Boros <Bpower@tests-MBP.attlocal.net>
2021-02-22 19:03:40 -0600

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2921bd9491055fca0cd65ab43ee4e0a1'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ef0280f6-9d69-4e57-b1f7-5d3b7c13d6a9'
+};
